86ca2813dc615649addbf867da7d4146b0ed93af
Sam Kreter <samkreter@gmail.com>
2016-11-02 15:06:14 -0500

    fixed send all sending to yourself

--- src/server2.cpp
+++ src/server2.cpp
@@ -336,8 +336,10 @@ int sendMessage(string cmd, int new_fd, const string& currUser, vector<cUser>& c
     if(username == string("all")){
         msg = currUser + ": " + msg;
         for(cUser user : currUsers){
-            msgs[user.id] = msg;
-            msgFlags[user.id] = true;
+            if(user.username != currUser){
+                msgs[user.id] = msg;
+                msgFlags[user.id] = true;
+            }
         }
         return 1;
     }
